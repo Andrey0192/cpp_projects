8aa47d1b66a39c96b3359d6720a37d81c43bab2b
bichevskiy <45871629+Andrey0192@users.noreply.github.com>
2025-01-22 18:00:44 +0700

    Update Reader.h

--- lab0/Reader.h
+++ lab0/Reader.h
@@ -1,34 +1,20 @@
-//
-// Created by PC on 13.10.2024.
-//
 
-#ifndef LAB0_READER_H
-#define LAB0_READER_H
-#include "string"
-#include "fstream"
-#include "list"
-#include "string"
-using std::string;
-using std::pair;
-using std::list;
-
-
+#pragma once
+#include <string>
+#include <fstream>
 class Reader {
-    string file_name_;
+    std::string file_name_;
     std::ifstream show_file_;
-
 public:
-    Reader(string file_name) : file_name_(file_name)   {
+    explicit Reader(std::string file_name) : file_name_(file_name) {
         show_file_.open(file_name_);
-
     }
+
     ~Reader() {
-        if (show_file_.is_open()){
+        if (show_file_.is_open()) {
             show_file_.close();
         }
     }
-   bool ReadLine(string& line);
+
+    bool ReadLine(std::string& line);
 };
-
-
-#endif //LAB0_READER_H
