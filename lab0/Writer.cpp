ef1b5d70a8ae2adc0e7fecf021d4ff1d091f5a54
bichevskiy <45871629+Andrey0192@users.noreply.github.com>
2025-01-22 18:01:00 +0700

    Update Writer.cpp

--- lab0/Writer.cpp
+++ lab0/Writer.cpp
@@ -1,22 +1,16 @@
-//
-// Created by PC on 13.10.2024.
-//
+
 
 #include "Writer.h"
-#include "fstream"
-#include "string"
-#include "list"
-#include "string"
-using std::string;
-using std::pair;
-using std::list;
-#define KPERCENT 100
+#include <fstream>
+#include <string>
+#include <list>
 
+#define PERCENT 100
 
-void Writer::Write(std::list<std::pair<std::string, unsigned int>> list_map, double number_of_words) {
+void Writer::Write(const std::list<std::pair<std::string, unsigned>>& list_map, double number_of_words) {
     if (write_file_.is_open()) {
-        for (auto& pair:list_map) {
-            write_file_ << pair.first << ";" << pair.second << ";" << (pair.second / number_of_words) * KPERCENT << std::endl;
+        for (const auto& pair : list_map) {
+            write_file_ << pair.first << ";" << pair.second << ";" << (static_cast<double>(pair.second) / number_of_words) * PERCENT << std::endl;
         }
     }
 }
