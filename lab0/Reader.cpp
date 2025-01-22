2fcec362ac0172ae0462dcc7eaf28ab41e899e6b
bichevskiy <45871629+Andrey0192@users.noreply.github.com>
2025-01-22 18:00:15 +0700

    Update Reader.cpp

--- lab0/Reader.cpp
+++ lab0/Reader.cpp
@@ -1,24 +1,8 @@
-//
-// Created by PC on 13.10.2024.
-//
 
 #include "Reader.h"
-#include "fstream"
-#include "string"
-#include "list"
-#include "string"
-using std::string;
-using std::pair;
-using std::list;
+#include <string>
 
-bool Reader::ReadLine(std::string &line) {
-    if (std::getline(show_file_,line) ){
-        return true;
-    }
-    return false;
+bool Reader::ReadLine(std::string& line) {
+    return static_cast<bool>(std::getline(show_file_, line));
 }
 
-
-//string Reader::GetFileName() {
-//    return file_name_;
-//}
