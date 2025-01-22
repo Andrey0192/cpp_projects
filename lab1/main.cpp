911efb00782fc0404f4b9f96fcdc208f354148b5
bichevskiy <45871629+Andrey0192@users.noreply.github.com>
2025-01-22 18:26:04 +0700

    Update main.cpp

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
-#include <cassert>
-#include "bitarray.h"
-
+#include "bitarray.cpp"
 int main() {
+    std::cout << "Hello, World!" << std::endl;
+    auto bitarray = BitArray(4,0b1101);
+    std::cout<<bitarray.to_string()<<"\n";
+    bitarray.operator|=(BitArray(4,4));
+    bitarray.resize(8);
+    std::cout<<bitarray.to_string()<<"\n";
+
+
 
     return 0;
 }
