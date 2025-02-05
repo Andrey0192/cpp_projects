44e60be2524a40ac114e542a344a238d5e6f2f84
bichevskiy <45871629+Andrey0192@users.noreply.github.com>
2025-02-05 19:03:08 +0700

    Update PlatformTile.cpp

--- PlatformTile.cpp
+++ PlatformTile.cpp
@@ -1,10 +1,10 @@
 #include "PlatformTile.h"
 #include <QBrush>
-#include <QPen>  // Добавлено включение QPen
+#include <QPen>  
 
 PlatformTile::PlatformTile(qreal x, qreal y, qreal width, qreal height)
         : QGraphicsRectItem(x, y, width, height)
 {
     setBrush(QBrush(Qt::darkGray));
-    setPen(QPen(Qt::NoPen));  // Передаём объект QPen
+    setPen(QPen(Qt::NoPen));  
 }
